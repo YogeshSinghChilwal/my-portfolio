127cbde566fa11984f050c23b4c21a9e9eba929d
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2025-05-10 18:13:03 +0530

    updated details section

--- components/details/details.tsx
+++ components/details/details.tsx
@@ -1,4 +1,4 @@
-import { Github, Twitter, Linkedin } from "lucide-react";
+import { Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 import Skills from "../skills/skills";
 import Avatar from "../avatar/avatar";
@@ -20,11 +20,11 @@ const Details = () => {
           <div className="mt-1">
             <p>
               <span className="text-gray-400 text-sm">
-                Buiding products that Earns.
+                Building Web Applications That Matter.
               </span>
             </p>
           </div>
-          <div className="flex gap-3 justify-center mt-2">
+          <div className="flex gap-3 justify-center items-center mt-2">
             <div className="text-gray-400 hover:text-white transition duration-300 ease-in-out transform">
               <Link
                 href={"https://github.com/YogeshSinghChilwal"}
@@ -33,17 +33,24 @@ const Details = () => {
                 <Github />
               </Link>
             </div>
-            <div className="text-gray-400 hover:text-white transition duration-300 ease-in-out transform">
+            <div className="text-gray-400 hover:text-white ">
               <Link
-                href={"https://github.com/YogeshSinghChilwal"}
+                href={"https://x.com/_chilwal"}
                 target="_blank"
               >
-                <Twitter />
+                <svg
+                  role="img"
+                  className="text-gray-400 w-5 h-5 hover:text-white transition duration-300 ease-in-out transform"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+                </svg>
               </Link>
             </div>
             <div className="text-gray-400 hover:text-white transition duration-300 ease-in-out transform">
               <Link
-                href={"https://github.com/YogeshSinghChilwal"}
+                href={"https://www.linkedin.com/in/yogesh-singh-chilwal-7b9a5a35b/"}
                 target="_blank"
               >
                 <Linkedin />
